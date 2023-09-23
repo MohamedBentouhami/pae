0a761929b9771f6b15dc8d30ddc6be80755bb4cf
Mohamed Bentouhami Belhaj <56387@etu.he2b.be>
2023-09-23 14:23:12 +0200

    add commun layout

--- src/main/java/com/example/demo/web/CourseController.java
+++ src/main/java/com/example/demo/web/CourseController.java
@@ -3,8 +3,10 @@ package com.example.demo.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.business.PAE;
+import com.example.demo.model.Course;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,9 +21,19 @@ public class CourseController {
 
     @GetMapping("/courses")
     public String getCourses(Model model){
-        log.debug("Course Size", this.business.getCourses().size());
-        model.addAttribute("courses", business.getCourses());
+        model.addAttribute("courses", this.business.getCourses());
+        model.addAttribute("course", new Course());
         return "courses";
+    }
+    @GetMapping("/courses/1")
+    public String getDetailCourse(Model model){
+        model.addAttribute("courses", this.business.getCourses());
+        return "detail_course";
+    }
+    @PostMapping("/addCourse")
+    public String addCourse(Course newCourse){
+        log.info(newCourse.toString());
+        return "home";
     }
     
 }
